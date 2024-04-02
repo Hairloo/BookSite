a5e94c6ce1d1f36d48658a1a2ad894e81074e522
wannacry <lobanov4747@gmail.com>
2024-04-02 14:15:17 +0300

    book repo added

---
+++ src/main/java/com/example/booksite/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.booksite.repositories;
+
+import com.example.booksite.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByBookName(String bookName);
+}
